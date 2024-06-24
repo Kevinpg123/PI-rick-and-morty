e062c1dff491db452f6549c87c0d6e0f05af8418
Kevin Osmar Pauluk <osmarkevinp@gmail.com>
2024-06-24 10:55:48 -0300

    postFav arreglado

--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -1,16 +1,25 @@
-const { Favorite } = require('../DB_connection');
+const { Favorite, User } = require('../DB_connection');
 
 const postFav = async(req, res) => {
-    const { name, origin, status, image, species, gender } = req.body;
+    const { name, origin, status, image, species, gender, userID } = req.body;
     
     try {
-        if (!name || !origin || !status || !image || !species || !gender) { return res.status(400).json({ message: "Faltan datos" }) };
+        if (!name || !origin || !status || !image || !species || !gender || !userID) { return res.status(400).json({ message: "Faltan datos" }) };
         const [favorite, created] = await Favorite.findOrCreate({
             where: {
                 name, origin, status, image, species, gender
             }
         })
-        if (created) { return res.status(201).json(favorite) }
+        
+        const user = await User.findByPk(userID);
+        if (!user) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        // Añadir el favorito al usuario
+        await user.addFavorite(favorite);
+
+        return res.status(201).json(favorite);
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
